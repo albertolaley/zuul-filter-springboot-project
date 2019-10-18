d6293f763d49ad3d18e66af689aa8cc21af897a0
albertorosario <alberto_rosario@claro.com.do>
2019-10-18 14:23:16 -0400

    Adding gateway service

---
+++ gatewayservice/src/main/java/com/albertorosario/springboot_microservices/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,33 @@
+package com.albertorosario.springboot_microservices.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayserviceApplication.class, args);
+  }
+
+  @Bean
+  public PreFilter preFilter() {
+    return new PreFilter();
+  }
+  @Bean
+  public PostFilter postFilter() {
+    return new PostFilter();
+  }
+  @Bean
+  public ErrorFilter errorFilter() {
+    return new ErrorFilter();
+  }
+  @Bean
+  public RouteFilter routeFilter() {
+    return new RouteFilter();
+  }
+
+}
---
+++ gatewayservice/src/test/java/com/albertorosario/springboot_microservices/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertorosario.springboot_microservices.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
